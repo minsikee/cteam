1ade2522f8a357e74090036241180199e4c7f0ce
gyeong-seok-Oh <dhrudtjr945@gmail.com>
2020-08-18 12:23:13 +0900

    FindStore 수정

--- cteam/app/src/main/java/com/example/cteam/WalkBoard.java
+++ cteam/app/src/main/java/com/example/cteam/WalkBoard.java
@@ -29,42 +29,7 @@ public class WalkBoard extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-      //spinner 객체 생성
 
- /*
-        ArrayAdapter yearAdapter = ArrayAdapter.createFromResource(this, board_SpinnerArray,android.R.layout.simple_spinner_dropdown_item);
-        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        board_spinner.setAdapter(yearAdapter);
-*/
-
-
-
-        //values에 있는 array.xml의 item을 String 배열로 가져오기
-        //String[] str = getResources().getStringArray(R.array.board_SpinnerArray);
-
-        //spinner_item.xml과 str을 인자로 어댑터 생성.
-        /*final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.spinner_item, str);
-        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        board_spinner.setAdapter(adapter);*/
-
-
-
-        //spinner 이벤트 리스너
-     /*   board_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                if (board_spinner.getSelectedItemPosition() > 0) {
-                    //선택된 항목
-                    Log.v("알림", board_spinner.getSelectedItem().toString() + "is selected");
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-            }
-
-        });*/
     }
 
     @Override
