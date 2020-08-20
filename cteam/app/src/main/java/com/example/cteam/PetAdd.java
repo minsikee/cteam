20fe173ba21bdb245f66e37e41df63f1c3474c65
red11024 <red1102438@gmail.com>
2020-08-20 10:12:44 +0900

    aaa

--- cteam/app/src/main/java/com/example/cteam/PetAdd.java
+++ cteam/app/src/main/java/com/example/cteam/PetAdd.java
@@ -8,23 +8,21 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.cteam.pet_add.petInsert;
-import com.example.cteam.pet_add.petUpdate;
 
 public class PetAdd extends AppCompatActivity {
 
-    Button button1;
-    Button button2;
+    Button btnAdd;
+    Button btnUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pet_add_insert);
+        setContentView(R.layout.activity_pet_add);
 
-        button1 = findViewById(R.id.button1);
-        button2 = findViewById(R.id.button2);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnUpdate = findViewById(R.id.btnUpdate);
 
-
-        button1.setOnClickListener(new View.OnClickListener() {
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), petInsert.class);
@@ -32,15 +30,10 @@ public class PetAdd extends AppCompatActivity {
             }
         });
 
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), petUpdate.class);
-                startActivity(intent);
-            }
-        });
+
 
     }
+
 
 
 
--- cteam/app/src/main/java/com/example/cteam/pet_add/petInsert.java
+++ cteam/app/src/main/java/com/example/cteam/pet_add/petInsert.java
@@ -1,6 +1,8 @@
 package com.example.cteam.pet_add;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +11,16 @@ import com.example.cteam.R;
 
 public class petInsert extends AppCompatActivity {
 
+    EditText petName, petAge,petWeight,petGender;
+    Button btnLoad;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_add_insert);
+
+
+
     }
 }
