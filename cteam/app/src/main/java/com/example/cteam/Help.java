375702aa24348a6d097b7c43730f6d988910049b
jinyi0604 <jinyi0604@naver.com>
2020-08-26 18:27:57 +0900

    메인에드로워/회원정보보기(일단 들어가는 비밀번호는 0000입니다) -> 수정할 예정

--- cteam/app/src/main/java/com/example/cteam/Help.java
+++ cteam/app/src/main/java/com/example/cteam/Help.java
@@ -1,14 +1,55 @@
-package com.example.cteam;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.lasttest;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class Help extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_help);
+import com.example.cteam.HelpAdapter;
+import com.example.cteam.PetSelect;
+import com.example.cteam.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Help extends Fragment {
+    PetSelect activity;
+    private RecyclerView recyclerview;
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.activity_help,container,false);
+
+        activity = (PetSelect) getActivity();
+
+        recyclerview = rootView.findViewById(R.id.recyclerView);
+        recyclerview.setLayoutManager(new LinearLayoutManager(activity.getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+        List<HelpAdapter.Item> data = new ArrayList<>();
+
+        HelpAdapter.Item list1 = new HelpAdapter.Item(HelpAdapter.HEADER, "반려동물은 어디서 데려오나요?");
+        list1.invisibleChildren  = new ArrayList<>();
+        list1.invisibleChildren .add(new HelpAdapter.Item(HelpAdapter.CHILD,
+                "사지말고 입양하세요"));
+        data.add(list1);
+
+        HelpAdapter.Item list2 = new HelpAdapter.Item(HelpAdapter.HEADER, "어플의 사용법");
+        list2.invisibleChildren  = new ArrayList<>();
+        list2.invisibleChildren .add(new HelpAdapter.Item(HelpAdapter.CHILD, "보다보면알아요"));
+        data.add(list2);
+
+        HelpAdapter.Item list3 = new HelpAdapter.Item(HelpAdapter.HEADER, "다운로드 받는 법");
+        list3.invisibleChildren  = new ArrayList<>();
+        list3.invisibleChildren .add(new HelpAdapter.Item(HelpAdapter.CHILD, "플레이스토어로 가서 받아요"));
+        data.add(list3);
+
+        recyclerview.setAdapter(new HelpAdapter(data));
+
+        return rootView;
     }
 }
--- cteam/app/src/main/java/com/example/cteam/PetSelect.java
+++ cteam/app/src/main/java/com/example/cteam/PetSelect.java
@@ -1,9 +1,14 @@
 package com.example.cteam;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.Manifest;
 import android.app.Activity;
@@ -11,18 +16,32 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.cteam.board.WalkBoard;
+import com.example.lasttest.Help;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 
-public class PetSelect extends AppCompatActivity {
+public class PetSelect extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    public static MemberDTO dto = null;
     Calendar calendar;
     WalkBoard walkBoard;
     FindStore findStore;
     PetPhoto petPhoto;
-
+    Toolbar toolbar;
+    MyPage myPage;
+    Help help;
+    Logout logout;
+    PetChoose petChoose;
+    Pw_MyPage_Fragment pw_myPage_fragment;
+    // MemberDTO dto;
+    Bundle mBundle = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +52,39 @@ public class PetSelect extends AppCompatActivity {
         walkBoard = new WalkBoard();
         petPhoto = new PetPhoto();
         findStore = new FindStore();
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                0, 0);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        petChoose = new PetChoose();
+        pw_myPage_fragment = new Pw_MyPage_Fragment();
+        myPage = new MyPage();
+        help = new Help();
+        logout = new Logout();
+
+        //MemberDTO
+        dto = new MemberDTO("jamong", "자몽", "0000", "보물 1호?", "나","010-0000-0000");
+
+        //frameLayout 초기화
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, petChoose).commit();
+
+        //드로워에 로그인 정보 표시하기
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        String loginID = "자몽";
+        View headerView = navigationView.getHeaderView(0);
+        ImageView imageView = headerView.findViewById(R.id.profile);
+        TextView tvloginID = headerView.findViewById(R.id.user_id);
+        TextView tvloginName = headerView.findViewById(R.id.user_name);
+        tvloginID.setText(dto.getId());
+        tvloginName.setText(dto.getName());
+        imageView.setImageResource(R.drawable.dog);
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.container, calendar).commit(); //첫화면에 프래그먼트 1이 나오게
@@ -65,8 +117,68 @@ public class PetSelect extends AppCompatActivity {
             }
         });//선택됐을때
 
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
 
+        //선택된 아이디를 얻는다
+        int id = menuItem.getItemId();
+
+        if(id == R.id.nav_myPage){
+            Bundle bundle = new Bundle();
+            bundle.putSerializable("memberdto", dto);
+
+            onFragmentSelected(0, bundle);
+        }else if(id == R.id.nav_help){
+            onFragmentSelected(1, null);
+        }else if(id == R.id.nav_logout){
+            onFragmentSelected(2, null);
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);        //메뉴 누른뒤에 사라지는 것(스타트상태로)
+        return true;
+    }
+
+    //프레그먼트가 선택 되었을 때 화면전환
+    //번들은 보낼 데이터가 있을때 사용
+    public void onFragmentSelected(int position, Bundle bundle){
+        Fragment curFragment = null;
+
+        if (position == 0){
+            mBundle = bundle;
+            curFragment = pw_myPage_fragment;
+            toolbar.setTitle("마이페이지");
+        }else if (position == 1){
+            curFragment = help;
+            toolbar.setTitle("도움말");
+        }else if (position == 2){
+            curFragment = logout;
+            toolbar.setTitle("로그아웃");
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,curFragment).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    public void onFragmentChange(int state, Bundle bundle){
+        if(state == 1){
+            mBundle = bundle;
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, myPage).commit();
+        }else if(state == 2){
+            mBundle = bundle;
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, petChoose).commit();
+        }
     }
 
     private void checkDangerousPermissions() {  //위험권한
