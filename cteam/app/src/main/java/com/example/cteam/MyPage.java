c935bd3166d7f70b157f8b36785c7224f5d486b0
jinyi0604 <jinyi0604@naver.com>
2020-09-17 20:42:04 +0900

    no message

--- cteam/app/src/main/java/com/example/cteam/MyPage.java
+++ cteam/app/src/main/java/com/example/cteam/MyPage.java
@@ -4,17 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.cteam.ATask.MypageUpdate;
+
+import java.io.IOException;
+
 
 public class MyPage extends Fragment {
     PetSelect activity;
-
-
+    Button MyPage_ok;
+    String member_id;
+    String member_pw;
+    String member_name;
+    String member_phonenum;
+    String member_question;
+    String member_answer;
 
     @Nullable
     @Override
@@ -23,6 +34,7 @@ public class MyPage extends Fragment {
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.activity_my_page,container,false);
 
         TextView myPage_name;
+        TextView myPage_pw;
         TextView myPage_id;
         TextView myPage_qs;
         TextView myPage_qs_as;
@@ -32,6 +44,8 @@ public class MyPage extends Fragment {
         myPage_name.setText(Login.loginDTO.getMember_name());
         myPage_id = rootView.findViewById(R.id.MyPage_id);
         myPage_id.setText(Login.loginDTO.getMember_id());
+        myPage_pw = rootView.findViewById(R.id.MyPage_pw);
+        myPage_pw.setText(Login.loginDTO.getMember_pw());
         myPage_qs = rootView.findViewById(R.id.MyPage_qs);
         myPage_qs.setText(Login.loginDTO.getMember_qeustion());
         myPage_qs_as = rootView.findViewById(R.id.MyPage_qs_as);
@@ -39,8 +53,29 @@ public class MyPage extends Fragment {
         myPage_phonenum = rootView.findViewById(R.id.MyPage_phonenum);
         myPage_phonenum.setText(Login.loginDTO.getMember_phonenum());
 
+        MyPage_ok = rootView.findViewById(R.id.MyPage_ok);
+        MyPage_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                member_id = myPage_id.getText().toString();
+                member_pw = myPage_pw.getText().toString();
+                member_name = myPage_name.getText().toString();
+                member_question = myPage_qs.getText().toString();
+                member_answer = myPage_qs_as.getText().toString();
+                member_phonenum = myPage_phonenum.getText().toString();
+
+                MypageUpdate mypageUpdate = null;
+                try {
+                    mypageUpdate = new MypageUpdate(member_id,member_pw,member_name, member_question,member_answer,member_phonenum);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                mypageUpdate.execute();
+            }
+        });
 
         return rootView;
+
     }
 
 }
