dc65e2e821892fe18f7e3736b7e82743ffe3cc4f
sy0435 <sycomkk@gmail.com>
2020-09-22 14:45:30 +0900

    zzz

--- cteam/app/src/main/java/com/example/cteam/Calendar.java
+++ cteam/app/src/main/java/com/example/cteam/Calendar.java
@@ -1,30 +1,35 @@
 package com.example.cteam;
 
-import android.content.ClipData;
-import android.graphics.Bitmap;
-import android.graphics.Color;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.view.menu.MenuView;
 import androidx.fragment.app.Fragment;
 
-import android.provider.CalendarContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.ImageView;
+
+import android.widget.Toast;
 
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.EventDay;
 import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
+import com.example.cteam.ATask.CalcalSelect;
 import com.example.cteam.Dto.CalendarDTO;
 
-
+import java.text.SimpleDateFormat;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static com.example.cteam.Common.CommonMethod.isNetworkConnected;
 
 public class Calendar extends Fragment {
     //로그 찍어볼 때
@@ -38,6 +43,11 @@ public class Calendar extends Fragment {
     com.applandeo.materialcalendarview.CalendarView CalendarView;
     List<EventDay> events;
     ImageView Calendar_event;
+
+    ArrayList<CalendarDTO> icons;
+    CalcalSelect calcalSelect;
+
+    String date;
 
     @Nullable
     @Override
@@ -69,10 +79,13 @@ public class Calendar extends Fragment {
             @Override
             public void onDayClick(EventDay eventDay) {
                 //날짜 데이터 보내는 부분
-                String select_date = eventDay.getCalendar().getTime().toString();
-                //Log.d(TAG, "onDayClick: " + select_date);
+                SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh");
+                String select_date = format.format(eventDay.getCalendar().getTime());
+                //String select_date = eventDay.getCalendar().getTime().toString();
                 Bundle bundle = new Bundle();
                 bundle.putString("select_date", select_date);
+
+                Log.d("main:Calendar", "onCreateView: " + select_date);
 
                 //이벤트 추가 버튼 등장
                 Calendar_event = rootView.findViewById(R.id.Calendar_event);
@@ -88,82 +101,112 @@ public class Calendar extends Fragment {
             }
         });
 
-        //아이콘 추가1 >> 한개만
-        java.util.Calendar calendar1 = java.util.Calendar.getInstance();
-        calendar1.add(java.util.Calendar.DAY_OF_MONTH, 1);
-        events.add(new EventDay(calendar1, R.drawable.icon11));
+        //스케줄 데이터 불러옴
+        if(isNetworkConnected(getActivity()) == true) {
+            calcalSelect = new CalcalSelect();
+            try {
+                icons = calcalSelect.execute().get();
+                //Log.d("main:Calendar", "onCreateView: " + icons.get(0).calendar_date);
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(getActivity(), "인터넷이 연결되어 있지 않습니다.",
+                    Toast.LENGTH_SHORT).show();
+        }
 
-        //아이콘 추가2 >> 한개만
-        java.util.Calendar calendar2 = java.util.Calendar.getInstance();
-        calendar2.add(java.util.Calendar.DAY_OF_MONTH, 2);
-        events.add(new EventDay(calendar2, R.drawable.icon21));
-        events.add(new EventDay(calendar2, R.drawable.icon22));
+        //아이콘 붙이기
+        //String icon_date = getCalendar().getTime().toString();
+        //calendar_date = icon_date;
+        //icons.get(0).calendar_date = icon_date;
 
-        //아이콘 추가3 >> 한개만
+        LayerDrawable cal_icon3 = (LayerDrawable) getResources().getDrawable(R.drawable.cal_3icons);
+
+        cal_icon3.setDrawableByLayerId(R.id.cal_3icons1, getResources().getDrawable(R.drawable.icon11));
+        cal_icon3.setDrawableByLayerId(R.id.cal_3icons2, getResources().getDrawable(R.drawable.icon33));
+        cal_icon3.setDrawableByLayerId(R.id.cal_3icons3, getResources().getDrawable(R.drawable.icon44));
+
         java.util.Calendar calendar3 = java.util.Calendar.getInstance();
-        calendar3.add(java.util.Calendar.DAY_OF_MONTH, 3);
-        events.add(new EventDay(calendar3, R.drawable.icon41, R.drawable.icon42));
+        calendar3.add(java.util.Calendar.DAY_OF_MONTH, 0);
+        events.add(new EventDay(calendar3, R.drawable.cal_3icons));
 
-        //아이콘 추가4 >> 4+4일뒤로 설정됨, 한개만됨
-        //java.util.Calendar calendar4 = java.util.Calendar.getInstance();
-        //calendar4.add(java.util.Calendar.DAY_OF_MONTH, 4);
-        //events.add(new EventDay(calendar4, R.drawable.icon51));
-        //calendar4.add(java.util.Calendar.DAY_OF_MONTH, 4);
-        //events.add(new EventDay(calendar4, R.drawable.icon52));
+        //날짜 포맷 변경
+        //SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh");
 
-        //아이콘 추가5 >> 위치지정은 되나 한개만...
-        java.util.Calendar calendar5 = java.util.Calendar.getInstance();
-        calendar5.add(java.util.Calendar.DAY_OF_MONTH, 5);
-        events.add(new EventDay(calendar5, (R.drawable.cal_icon1 & R.drawable.cal_icon2)));
+        //String firstDate = icons.get(3).calendar_date;
+        //String secondDate = format.format(calendar3.getTime());
+/*
+        Date tempDate = new Date();
+        firstDate = new DecimalFormat("0000").format(tempDate.getYear()) + "-" +
+                new DecimalFormat("00").format(tempDate.getMonth() + 1) + "-" +
+                new DecimalFormat("00").format(tempDate.getDay());
+*/
+        //Log.d("main:Calendar", "onCreateView: " + select_date);
 
-        //아이콘 추가6 >> 하나만됨
-        java.util.Calendar calendar6 = java.util.Calendar.getInstance();
-        calendar6.add(java.util.Calendar.DAY_OF_MONTH, 6);
-        events.add(new EventDay(calendar6, R.drawable.cal_icon1));
-        events.add(new EventDay(calendar6, R.drawable.cal_icon2));
 
-        //아이콘 추가7 >> 오류
-        //java.util.Calendar calendar7 = java.util.Calendar.getInstance();
-        //calendar7.add(java.util.Calendar.DAY_OF_MONTH, 7);
-        //events.add(new EventDay(calendar7, R.drawable.cal_icon1 + R.drawable.cal_icon2));
 
-        //이렇게 추가해보기8 >> 추가는 된당ㅋㅋ
-        //int imgv1 = R.drawable.cal_icon1 + R.drawable.cal_icon2;
-        int imgv1 = R.drawable.cal_icon1;
-        java.util.Calendar calendar8 = java.util.Calendar.getInstance();
-        calendar8.add(java.util.Calendar.DAY_OF_MONTH, 8);
-        events.add(new EventDay(calendar8, imgv1));
+        //long calDate = firstDate.getTime() - secondDate.getTime();
+            //long calDates = calDate / (24*60*60*1000);
 
-        //이렇게 추가해보기9 >> 오류
-        //int imgv2 = Integer.parseInt(String.valueOf(R.drawable.cal_icon1) + String.valueOf(R.drawable.cal_icon2));
-        //String imgv2 = (String.valueOf(R.drawable.cal_icon1)) + (String.valueOf(R.drawable.cal_icon2));
-        //java.util.Calendar calendar9 = java.util.Calendar.getInstance();
-        //calendar9.add(java.util.Calendar.DAY_OF_MONTH, 9);
-        //events.add(new EventDay(calendar9, Integer.parseInt(imgv2)));
 
-        //이렇게 추가해보기10 >> 안됨
-        int imgv3 = R.drawable.cal_icon1;
-        int imgv4 = R.drawable.cal_icon2;
-        int imgv5 = imgv3 & imgv4;
-        java.util.Calendar calendar10 = java.util.Calendar.getInstance();
-        calendar10.add(java.util.Calendar.DAY_OF_MONTH, 10);
-        events.add(new EventDay(calendar10, imgv5));
 
-        //이렇게 추가해보기
-        //java.util.Calendar calendar11 = java.util.Calendar.getInstance();
-        //calendar11.add(java.util.Calendar.DAY_OF_MONTH, 11);
-        //events.add(new EventDay(calendar11, R.drawable.cal_icon1));
-        //events.add(R.drawable.cal_icon2);
-        //events.add(new EventDay(calendar11, R.drawable.cal_icon2));
+        /*
+        if(icons.get(0).calendar_date != null) {
+            cal_icon3.setDrawableByLayerId(R.id.cal_3icons1, getResources().getDrawable(R.drawable.icon11));
+            cal_icon3.setDrawableByLayerId(R.id.cal_3icons2, getResources().getDrawable(R.drawable.icon33));
+            cal_icon3.setDrawableByLayerId(R.id.cal_3icons3, getResources().getDrawable(R.drawable.icon44));
 
-        //이렇게 추가해보기
-        String imgv10 = String.valueOf(R.layout.fragment_calendar_view);
-        java.util.Calendar calendar12 = java.util.Calendar.getInstance();
-        calendar12.add(java.util.Calendar.DAY_OF_MONTH, 12);
-        events.add(new EventDay(calendar12, Integer.parseInt(imgv10)));
+            java.util.Calendar calendar3 = java.util.Calendar.getInstance();
+            calendar3.add(java.util.Calendar.DAY_OF_MONTH, 0);
+            events.add(new EventDay(calendar3, R.drawable.cal_3icons));
+        } else {
+            cal_icon3.setDrawableByLayerId(R.id.cal_3icons1, getResources().getDrawable(R.drawable.sample_icon_1));
+            cal_icon3.setDrawableByLayerId(R.id.cal_3icons2, getResources().getDrawable(R.drawable.sample_icon_2));
+            cal_icon3.setDrawableByLayerId(R.id.cal_3icons3, getResources().getDrawable(R.drawable.sample_icon_3));
+
+            java.util.Calendar calendar3 = java.util.Calendar.getInstance();
+            calendar3.add(java.util.Calendar.DAY_OF_MONTH, 0);
+            events.add(new EventDay(calendar3, R.drawable.cal_3icons));
+        }
+
+
+        //날짜 구하기?
+        //int eventDay = 0 - Integer.parseInt(icon.calendar_date);
+
+/*
+        //날짜에 아이콘 붙이기 (3개짜리)
+        LayerDrawable cal_icon3 = (LayerDrawable) getResources().getDrawable(R.drawable.cal_3icons);
+
+        cal_icon3.setDrawableByLayerId(R.id.cal_3icons1, getResources().getDrawable(R.drawable.icon11));
+        cal_icon3.setDrawableByLayerId(R.id.cal_3icons2, getResources().getDrawable(R.drawable.icon33));
+        cal_icon3.setDrawableByLayerId(R.id.cal_3icons3, getResources().getDrawable(R.drawable.icon44));
+
+        java.util.Calendar calendar3 = java.util.Calendar.getInstance();
+        calendar3.add(java.util.Calendar.DAY_OF_MONTH, 0);
+        events.add(new EventDay(calendar3, R.drawable.cal_3icons));
+
+        //날짜에 아이콘 붙이기 (4개짜리)
+        LayerDrawable cal_icon4 = (LayerDrawable) getResources().getDrawable(R.drawable.cal_4icons);
+
+        cal_icon4.setDrawableByLayerId(R.id.cal_4icons1, getResources().getDrawable(R.drawable.icon11));
+        cal_icon4.setDrawableByLayerId(R.id.cal_4icons2, getResources().getDrawable(R.drawable.icon22));
+        cal_icon4.setDrawableByLayerId(R.id.cal_4icons3, getResources().getDrawable(R.drawable.icon33));
+        cal_icon4.setDrawableByLayerId(R.id.cal_4icons4, getResources().getDrawable(R.drawable.icon44));
+
+        java.util.Calendar calendar4 = java.util.Calendar.getInstance();
+        calendar4.add(java.util.Calendar.DAY_OF_MONTH, 0);
+        events.add(new EventDay(calendar4, R.drawable.cal_4icons));
+        */
 
         return rootView;
 
     } //onCreateView()
+
+
+
+
+
 
 }
