ea5ec3407a0308a3fea337b7832651131b9a5a8c
greatjj <nojobghost@gmail.com>
2020-09-28 20:19:39 +0900

    s

--- cteam/app/src/main/java/com/example/cteam/Adapter/PetPhotoAdapter.java
+++ cteam/app/src/main/java/com/example/cteam/Adapter/PetPhotoAdapter.java
@@ -119,7 +119,9 @@ public class PetPhotoAdapter extends RecyclerView.Adapter<PetPhotoAdapter.ItemVi
         }
 
         public void setItem(PetPhotoDTO petPhoto){
-
+            if(petPhoto!=null){
+                Log.d("yyyy", "setItem: d"+petPhoto.getPetPhoto_content());
+            }
            // id.setText(item.getId());
             petPhoto_petName.setText(petPhoto.getPetName());
             petPhoto_date.setText(petPhoto.getPetPhoto_date());
